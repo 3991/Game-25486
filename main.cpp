f32a54a9fadf2999b19eb7708a7dc60959594680
921 <courage_pour_tenir@hotmail.fr>
2015-06-09 21:28:08 +0100

    Init git

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "src/scenes/screens.hpp"
+
+int main(int argc, char** argv)
+{
+    //Applications variables
+    std::vector<cScreen*> Screens;
+    int screen = 0;
+
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+
+    //Window creation
+    sf::RenderWindow App(sf::VideoMode(800, 600, 32), "game 25486", sf::Style::Default, settings);
+
+    //Mouse cursor no more visible
+    App.setMouseCursorVisible(true);
+
+
+
+    //Screens preparations
+    screen_0 s0;
+    Screens.push_back(&s0);
+    screen_1 s1;
+    Screens.push_back(&s1);
+
+    //Main loop
+    while (screen >= 0)
+    {
+        screen = Screens[screen]->Run(App);
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ src/Manager.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "cScreen.hpp"
+#include <SFML/Graphics.hpp>
+#define FONT_SIZE 18
+
+class Manager {
+    private:
+
+    public:
+        Manager(void);
+};
+
+Manager::Manager(void) {
+}
+
+
+void Manager::progress(float frame){
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+
+
+    }
+}
